7d1b8d539fd3e0191646e70218f81364752624b7
Igor Minar <iiminar@gmail.com>
2011-04-19 10:54:44 -0700

    step-9 checkmark filter
    - Added custom checkmark filter
    - Update phone detail template to use checkmark filter
    - Added spec for the filter

--- app/js/app.js
+++ app/js/app.js
@@ -2,10 +2,20 @@
 
 /* App Module */
 
-var myApp = angular.module('phonecatApp', []).
-  config(['$routeProvider', function($routeProvider) {
-  $routeProvider.
-      when('/phones', {templateUrl: 'partials/phone-list.html',   controller: 'PhoneListCtrl'}).
-      when('/phones/:phoneId', {templateUrl: 'partials/phone-detail.html', controller: 'PhoneDetailCtrl'}).
-      otherwise({redirectTo: '/phones'});
-}]);
+var phonecatApp = angular.module('phonecatApp', ['phonecatFilters']);
+
+phonecatApp.config(['$routeProvider',
+  function($routeProvider) {
+    $routeProvider.
+      when('/phones', {
+        templateUrl: 'partials/phone-list.html',
+        controller: 'PhoneListCtrl'
+      }).
+      when('/phones/:phoneId', {
+        templateUrl: 'partials/phone-detail.html',
+        controller: 'PhoneDetailCtrl'
+      }).
+      otherwise({
+        redirectTo: '/phones'
+      });
+  }]);
--- app/js/controllers.js
+++ app/js/controllers.js
@@ -4,16 +4,18 @@
 
 var phonecatApp = angular.module('phonecatApp', []);
 
-phonecatApp.controller('PhoneListCtrl', function PhoneListCtrl($scope) {
-  $http.get('phones/phones.json').success(function(data) {
-    $scope.phones = data;
-  });
+phonecatApp.controller('PhoneListCtrl', ['$scope',
+  function($scope) {
+    $http.get('phones/phones.json').success(function(data) {
+      $scope.phones = data;
+    });
 
-  $scope.orderProp = 'age';
-});
+    $scope.orderProp = 'age';
+  }]);
 
-phonecatApp.controller('PhoneDetailCtrl', function PhoneDetailCtrl($scope, $routeParams, $http) {
-  $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
-    $scope.phone = data;
-  });
-});
+phonecatApp.controller('PhoneDetailCtrl', ['$scope', '$routeParams', '$http',
+  function($scope, $routeParams, $http) {
+    $http.get('phones/' + $routeParams.phoneId + '.json').success(function(data) {
+      $scope.phone = data;
+    });
+  }]);
